79be12c07dd80a014cbac8d4c18fc3b6b8d65965
delphinelepront <55401303+delphinelepront@users.noreply.github.com>
2020-08-27 12:36:54 +0200

    Update projects.php

--- projects.php
+++ projects.php
@@ -9,6 +9,39 @@ require 'models/groupModel.php';
 $users = getUsers();
 $projects = getProjects();
 $groups = getGroups();
+
+if(isset($_POST['addproject'])){
+    extract($_POST);
+    $errors = array();
+
+    $name = strip_tags($name);
+    $description = strip_tags($description);
+    $dateCreation = date("Y-m-d");
+    $statut = "In progress";
+    $statutProject = strip_tags($statut);
+    $idCreator = $_SESSION['id'];
+
+    if(empty($name)){
+        array_push($errors, 'Entrez un nom de projet');
+    }
+    if(empty($description)){
+        array_push($errors, 'Entrez une description');
+    }
+    if(count($errors) == 0){
+        $projectadded = addProject($name, $description, $dateCreation, $statutProject, $idCreator);
+
+        $success = 'Le projet a été créé';
+        unset($name);
+        unset($description);
+
+        $projectcreated = getProjectCreated($idCreator);
+        foreach ($projectcreated as $lid):
+            $lidd = $lid->id;
+        endforeach;
+        echo '<a href="project.php?id='.$lidd.'">Accèdez au projet !</a> Et assignez un groupe de travail au projet et commencer à créer des tâches dès maintenant';
+    }
+}
+
 ?>
 
 
