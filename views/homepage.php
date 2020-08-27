c1c0b4d92fdf5a8420f0d307dfc015fe97c87b99
LouisChevallier <42800047+LouisChevallier@users.noreply.github.com>
2020-08-27 12:23:57 +0200

    Update homepage.php
    Ajout de la fonction et boucles pour voir les projets auquels participe l'user

--- views/homepage.php
+++ views/homepage.php
@@ -15,9 +15,22 @@
                         <div class="projetsEnCours" style="border: solid; border-color: white; border-width: 1px; background-color: rgba(244, 248, 247, 0.5); padding-left: 5%; padding-right: 5%;">
                             <h2 style="text-align: left; margin-top: 10px;">Mes projets en cours :</h2>
                             <ul style="list-style-type: none;">
-                                <?php foreach ($idProjectUser as $projectUser): ?>
-                                    <li style="border: solid; padding-bottom: 10px; padding-top: 10px; border-width: 1px; border-radius: 30px; text-align: left; background-color: #1C846C; opacity: 0.5; color: white; font-size: 20px; padding-left: 10%;margin-bottom: 10px;"><a href="project.php?id=<?= $projectUser->id ?>"><?= $projectUser->name ?></a></li>
-                                <?php endforeach; ?>
+
+
+                                <?php foreach ($id_user as $id_us):
+                                    $id_groups = $id_us->id_groups;
+                                    $id_groups = getProjectIdInGroup($id_groups);
+                                    foreach ($id_groups as $id_gr):
+                                        $id_final = $id_gr->id_project;
+                                        $id_final = getProjectWithFinalId($id_final);
+                                        foreach ($id_final as $proj): ?>
+                                            <li style="border: solid; padding-bottom: 10px; padding-top: 10px; border-width: 1px; border-radius: 30px; text-align: left; background-color: #1C846C; opacity: 0.5; color: white; font-size: 20px; padding-left: 10%;margin-bottom: 10px;"><a href="project.php?id=<?= $proj->id ?>"><?= $proj->name ?></a></li>
+                                        <?php endforeach;
+                                    endforeach;
+                                endforeach; ?>
+
+
+
                             </ul>
                         </div>
                     <?php
