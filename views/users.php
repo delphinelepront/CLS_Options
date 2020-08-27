9c81e573d48187429716f175696da325b5ed279b
delphinelepront <55401303+delphinelepront@users.noreply.github.com>
2020-08-27 12:59:06 +0200

    Update users.php

--- views/users.php
+++ views/users.php
@@ -33,6 +33,35 @@
                             </table>
                         </div>
                         <h1>Ajouter un utlisateur : </h1>
+
+                        <?php
+                        if(isset($success)){
+                            echo $success;
+                        }
+
+                        if(!empty($errors)):?>
+                            <?php foreach($errors as $error): ?>
+                                <p><?= $error ?></p>
+                            <?php  endforeach; ?>
+                        <?php endif; ?>
+
+
+                        <form action="users.php" method="post">
+                            <p><label for="username">Pseudo :</label><br/>
+                                <input type="text" name="username" id="username" class="form-control" value="<?php if (isset($username)) echo $username ?>" /></p>
+
+                            <p><label for="prenom">Prenom :</label><br/>
+                                <input type="text" name="prenom" id="prenom" class="form-control" value="<?php if (isset($prenom)) echo $prenom ?>" /></p>
+
+                            <p><label for="nom">Nom :</label><br/>
+                                <input type="text" name="nom" id="nom" class="form-control" value="<?php if (isset($nom)) echo $nom ?>" /></p>
+
+                            <p><label for="mdp">Mot de passe :</label><br/>
+                                <input type="password" name="mdp" id="mdp" class="form-control" value="<?php if (isset($mdp)) echo $mdp ?>"/></p>
+                            <p><label for="mdp2">Confirmez le mot de passe :</label><br/>
+                                <input type="password" name="mdp2" id="mdp2" class="form-control"/></p>
+                            <button type="submit">S'inscrire</button>
+                        </form><br/>
                     </div><br/>
 
                 </div>
