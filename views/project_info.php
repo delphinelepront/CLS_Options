e677e9f4ecdf6ded727f8727209c89a71ef5106d
delphinelepront <55401303+delphinelepront@users.noreply.github.com>
2020-08-27 12:35:15 +0200

    Update project_info.php

--- views/project_info.php
+++ views/project_info.php
@@ -16,49 +16,106 @@ $superhoras = 0 ?>
                             <li>Description : <?= $project->description ?></li>
                             <li>Création le : <?= $project->dateCreation ?> par <a
                                         href="user.php?id=<?= $project->id_users ?>"><?= $creator->username ?></a></li>
+                            <li><h2>Groupe(s) assignés </h2>
+                                <?php foreach ($groupproj as $gj):
+                                    $idGroup = $gj->id_groups;
+                                    $groupID = getGroupInProject ($idGroup);
+                                    foreach ($groupID as $detGroup): ?>
+                                        <ul>
+                                            <li><a href="group.php?id=<?= $detGroup->id ?>"><?= $detGroup->nameGroup ?></a></li>
+                                            <li><?= $detGroup->description ?></li>
+
+                                            <?php
+                                            $idGroup = $detGroup->id;
+                                            $numberMember = countUsersInGroup ($idGroup)
+                                            ?>
+                                            <li>Nombre membres : <?= $numberMember->total ?></li>
+                                            <li><a href="group.php?id=<?= $detGroup->id ?>">Ajouter des membres</a></li>
+                                        </ul>
+                                    <?php endforeach;
+                                endforeach; ?></li>
                             <br>
                             <li>
                                 <?php foreach ($tasks as $task): ?>
 
-                            <?php
+                                    <?php
 
-                            $idTaskk = $task->id;
-                            $task_timerecorded = getAllTaskTimeRecorded($idTaskk);
-                            //var_dump($task_timerecorded);
-                            $totalintsec = 0;
-                            foreach ($task_timerecorded as $detail):
-                                $h1 = strtotime($detail->startTime);
-                                $h2 = strtotime($detail->stopTime);
-                                $Startsec = gmdate("U", $h2 - $h1);
-                                $intsec = intval($Startsec);
-                                if ($intsec > 0) {
-                                    $totalintsec = $totalintsec + $intsec;
-                                }
-                                $h1 = 0;
-                                $h2 = 0;
-                                $intsec = 0;
-                                $Startsec = 0;
-                            endforeach;
+                                    $idTaskk = $task->id;
+                                    $task_timerecorded = getAllTaskTimeRecorded($idTaskk);
+                                    //var_dump($task_timerecorded);
+                                    $totalintsec = 0;
+                                    foreach ($task_timerecorded as $detail):
+                                        $h1 = strtotime($detail->startTime);
+                                        $h2 = strtotime($detail->stopTime);
+                                        $Startsec = gmdate("U", $h2 - $h1);
+                                        $intsec = intval($Startsec);
+                                        if ($intsec > 0) {
+                                            $totalintsec = $totalintsec + $intsec;
+                                        }
+                                        $h1 = 0;
+                                        $h2 = 0;
+                                        $intsec = 0;
+                                        $Startsec = 0;
+                                    endforeach;
 
-                            /* puis on traduit en secondes */
-                            $horas = $totalintsec;
-                            //var_dump($horas);
+                                    /* puis on traduit en secondes */
+                                    $horas = $totalintsec;
+                                    //var_dump($horas);
 
-                            if ($horas > 0) {
-                                $TimeFinal = ConvertisseurTime($horas);
-                            }
+                                    if ($horas > 0) {
+                                        $TimeFinal = ConvertisseurTime($horas);
+                                    }
 
-                            $superhoras = $superhoras + $horas;
-                            ?>
-                            <?php endforeach; ?>
+                                    $superhoras = $superhoras + $horas;
+                                    ?>
+                                <?php endforeach; ?>
 
-                            <?php $TimeFinalALL = ConvertisseurTime($superhoras); ?>
-                            <div>
-                                <h2>Temps total sur ce projet : <?= $TimeFinalALL ?></h2>
-                            </div>
-
+                                <?php $TimeFinalALL = ConvertisseurTime($superhoras); ?>
+                                <div>
+                                    <h2>Temps total sur ce projet : <?= $TimeFinalALL ?></h2>
+                                </div>
+                            </li>
                     </div>
                     <br/>
+                    <div>
+                        <h2>Ajouter une tâche</h2>
+                        <div class="card-body">
+                            <form action="project_info.php?id=<?= $project->id ?>" method="post">
+                                <p><label for="title">Title :</label><br/>
+                                    <input type="text" name="title" id="title" class="form-control"
+                                           value="<?php if (isset($title)) echo $title ?>"/></p>
+                                <p><label for="textTask">textTask :</label><br/>
+                                    <textarea name="textTask" id="textTask" class="form-control" cols="30"
+                                              rows="4"/><?php if (isset($textTask)) echo $textTask ?></textarea></p>
+                                <button type="submit" name="addtask">Ajouter</button>
+                            </form>
+                        </div>
+                    </div>
+                    <br>
+                    <h2>Assigner le projet à un groupe</h2>
+                    <div class="card-body">
+                        <form action="project_info.php?id=<?= $project->id ?>" method="post">
+                            <label for="proj">Groupes :</label><br/>
+                            <select name="proj" id="proj">
+                                <?php foreach ($groups as $groupo): ?>
+                                    <option value="<?= $groupo->id ?>"><?= $groupo->nameGroup ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                            <button type="submit" name="addgrouptoproject">Ajouter</button>
+                        </form>
+                    </div>
+
+
+                    <?php
+                    if (isset($success)) {
+                        echo $success;
+                    }
+
+                    if (!empty($errors)):?>
+                        <?php foreach ($errors as $error): ?>
+                            <p><?= $error ?></p>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
 
                 </div>
             </div>
