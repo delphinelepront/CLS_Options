a6c9448e8dfbfac297ccfe261aaf941e54578b86
delphinelepront <55401303+delphinelepront@users.noreply.github.com>
2020-08-27 12:32:48 +0200

    Update project.php

--- views/project.php
+++ views/project.php
@@ -11,106 +11,114 @@ $superhoras = 0 ?>
                 <div class="home-info">
                     <div class="DetailsProject"
                          style="border: solid; border-color: white; border-width: 1px; background-color: rgba(244, 248, 247, 0.5)">
-                        <h3> <?= strtoupper($project->name) ?> (<?= $project->statutProject ?>) </li></h3>
+                        <h3> <?= strtoupper($project->name) ?> (<?= $project->statutProject ?>)</h3>
                         <ul style="list-style-type: none;">
                             <li>Description : <?= $project->description ?></li>
-                            <li>Création le : <?= $project->dateCreation ?> par <a href="user.php?id=<?= $project->id_users ?>"><?= $creator->username ?></a></li><br>
-                            <li>
-                                <?php foreach ($tasks as $task): ?>
-                            <li>Nom de la tâche :<?= $task->title ?></li>
-                            <li>Date : <?= $task->date ?></li>
-                            <li>
-                                <?php
-                                $takenornot = $task->statutTask;
-                                if ($takenornot == "takable") {
-                                    ?>
-                                    <h3>Lancez le chrono</h3>
-                                <form action="project.php?id=<?= $project->id ?>" method="post">
-                                    <button type="submit" name="startTask" value="<?= $task->id ?>">Start</button>
-                                    </form><?php } else { ?>
-                                    <h3>Stoppez le chrono</h3>
-                                    <form action="project.php?id=<?= $project->id ?>" method="post">
-                                        <button type="submit" name="stopTask" value="<?= $task->id ?>">Stop</button>
-                                    </form>
-                                <?php } ?>
-                            </li>
-
-                            <?php
-                            if (isset($_POST['startTask'])) {
-                                extract($_POST);
-                                $errors = array();
-
-                                $startTime = date("H:i:s");
-                                $id_users = $_SESSION['id'];
-                                $id_task = $_POST['startTask'];
-
-                                $taskStart = startTaskTimeRecorded($startTime, $id_users, $id_task);
-                                $statutTask = "taken";
-                                setTaken($statutTask, $id_task);
-                                $success = 'Votre tâche démarre';
-
-                                unset($startTime);
-                                unset($id_users);
-                                unset($id_task);
-                                header("Refresh:0");
-                            }
-
-                            if (isset($_POST['stopTask'])) {
-                                extract($_POST);
-                                $errors = array();
-
-                                $stopTime = date("H:i:s");
-                                $id_task = $_POST['stopTask'];
-
-                                $taskStart = stopTaskTimeRecorded($stopTime, $id_task);
-                                $statutTask = "takable";
-                                setTakable($statutTask, $id_task);
-                                $success = 'Votre tâche a été stoppé';
-
-                                unset($stopTime);
-                                unset($id_task);
-                                header("Refresh:0");
-                            }
-
-                            $idTaskk = $task->id;
-                            $task_timerecorded = getAllTaskTimeRecorded($idTaskk);
-                            //var_dump($task_timerecorded);
-                            $totalintsec = 0;
-                            foreach ($task_timerecorded as $detail):
-                                $h1 = strtotime($detail->startTime);
-                                $h2 = strtotime($detail->stopTime);
-                                $Startsec = gmdate("U", $h2 - $h1);
-                                $intsec = intval($Startsec);
-                                if ($intsec > 0) {
-                                    $totalintsec = $totalintsec + $intsec;
-                                }
-                                $h1 = 0;
-                                $h2 = 0;
-                                $intsec = 0;
-                                $Startsec = 0;
-                            endforeach;
-
-                            /* puis on traduit en secondes */
-                            $horas = $totalintsec;
-                            //var_dump($horas);
-
-                            if ($horas > 0) {
-                                $TimeFinal = ConvertisseurTime($horas);
-                            }
-
-                            $superhoras = $superhoras + $horas;
-                            ?>
-                            <li>Temps passé : <?= $TimeFinal ?></li>
-                            <?php endforeach; ?>
-
-                            <?php $TimeFinalALL = ConvertisseurTime($superhoras); ?>
-                            <div>
-                                <h2>Temps total sur ce projet : <?= $TimeFinalALL ?></h2>
-                            </div>
+                            <li>Création le : <?= $project->dateCreation ?> par <a
+                                        href="user.php?id=<?= $project->id_users ?>"><?= $creator->username ?></a></li>
+                            <br>
+                        </ul>
                     </div>
-                    <br/>
-                    </li>
-                    </ul>
+                    <br>
+                    <?php foreach ($tasks as $task): ?>
+                        <div class="DetailsTask"
+                             style="border: solid; border-color: white; border-width: 1px; background-color: rgba(244, 248, 247, 0.5)">
+                            <h3> <?= $task->title ?></h3>
+                            <ul style="list-style-type: none;">
+                                <li>Date : <?= $task->date ?></li>
+                                <li>
+                                    <?php
+                                    $takenornot = $task->statutTask;
+                                    if ($takenornot == "takable") {
+                                        ?>
+                                        <br>
+                                        <h3>Lancez le chrono</h3>
+                                    <form action="project.php?id=<?= $project->id ?>" method="post">
+                                        <button type="submit" name="startTask" value="<?= $task->id ?>">Start</button>
+                                        </form><?php } else { ?>
+                                        <h3>Stoppez le chrono</h3>
+                                        <form action="project.php?id=<?= $project->id ?>" method="post">
+                                            <button type="submit" name="stopTask" value="<?= $task->id ?>">Stop</button>
+                                        </form>
+                                    <?php } ?>
+                                </li>
+                                <li>
+
+                                    <?php
+                                    if (isset($_POST['startTask'])) {
+                                        extract($_POST);
+                                        $errors = array();
+
+                                        $startTime = date("H:i:s");
+                                        $id_users = $_SESSION['id'];
+                                        $id_task = $_POST['startTask'];
+
+                                        $taskStart = startTaskTimeRecorded($startTime, $id_users, $id_task);
+                                        $statutTask = "taken";
+                                        setTaken($statutTask, $id_task);
+                                        $success = 'Votre tâche démarre';
+
+                                        unset($startTime);
+                                        unset($id_users);
+                                        unset($id_task);
+                                        header("Refresh:0");
+                                    }
+
+                                    if (isset($_POST['stopTask'])) {
+                                        extract($_POST);
+                                        $errors = array();
+
+                                        $stopTime = date("H:i:s");
+                                        $id_task = $_POST['stopTask'];
+
+                                        $taskStart = stopTaskTimeRecorded($stopTime, $id_task);
+                                        $statutTask = "takable";
+                                        setTakable($statutTask, $id_task);
+                                        $success = 'Votre tâche a été stoppé';
+
+                                        unset($stopTime);
+                                        unset($id_task);
+                                        header("Refresh:0");
+                                    }
+
+                                    $idTaskk = $task->id;
+                                    $task_timerecorded = getAllTaskTimeRecorded($idTaskk);
+                                    //var_dump($task_timerecorded);
+                                    $totalintsec = 0;
+                                    foreach ($task_timerecorded as $detail):
+                                        $h1 = strtotime($detail->startTime);
+                                        $h2 = strtotime($detail->stopTime);
+                                        $Startsec = gmdate("U", $h2 - $h1);
+                                        $intsec = intval($Startsec);
+                                        if ($intsec > 0) {
+                                            $totalintsec = $totalintsec + $intsec;
+                                        }
+                                        $h1 = 0;
+                                        $h2 = 0;
+                                        $intsec = 0;
+                                        $Startsec = 0;
+                                    endforeach;
+
+                                    /* puis on traduit en secondes */
+                                    $horas = $totalintsec;
+                                    //var_dump($horas);
+
+                                    if ($horas > 0) {
+                                        $TimeFinal = ConvertisseurTime($horas);
+                                    }
+
+                                    $superhoras = $superhoras + $horas;
+                                    ?>
+                                <li>Temps passé : <?= $TimeFinal ?></li>
+                                <br>
+                            </ul>
+                        </div>
+                        <br>
+                    <?php endforeach; ?>
+                    <?php $TimeFinalALL = ConvertisseurTime($superhoras); ?>
+                    <div>
+                        <h2 style="color: white;">Temps total sur ce projet : <?= $TimeFinalALL ?></h2>
+                    </div>
                 </div>
             </div>
         </div>
@@ -118,3 +126,4 @@ $superhoras = 0 ?>
     </div>
     </div>
 </section>
+
